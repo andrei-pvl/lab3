31ecb5507a5ecc6919f8240eaf65d56e4df6314d
andrei-pvl <147505074+andrei-pvl@users.noreply.github.com>
2023-11-09 01:59:59 +0200

    Add files via upload

---
+++ FacadeComp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace comp
+{
+    public class FacadeComp : SingelComputer
+    {
+
+        private Switcher switcher { get; set; }
+        private Launcher launcher { get; set; }
+        private FileWork filework { get; set; }
+        
+        
+        public FacadeComp()
+        {
+            switcher = new Switcher();
+            launcher = new Launcher();
+            filework = new FileWork();
+        }
+        public override string ComputerName()
+        {
+            return "Iм'я комп'ютеру: Computer: Acer cc5532hk63";
+        }
+        public void TurnOn()
+        {
+            switcher.On();
+            launcher.LaunchOS();
+        }
+        public void ProgramWork()
+        {
+            Console.WriteLine(launcher.LaunchProgram());
+            
+            Console.WriteLine("Введiть назву файлу/програми");
+            filework.ScanFile();
+            Console.WriteLine("Введений файл/програма з назвою:");
+            Console.WriteLine(filework.PrintFile());
+           
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine(launcher.OffProgram());
+            Console.WriteLine(launcher.OffOS());
+            Console.WriteLine(switcher.Off());
+        }
+
+    }
+}
---
+++ FileWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace comp
+{
+    public class FileWork
+    {
+        private string file = "";
+
+        public void ScanFile()
+        {
+            file = Console.ReadLine();
+        }
+
+        public string PrintFile()
+        {
+            return file;
+        }
+    }
+}
