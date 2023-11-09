efdd3117168ac8686367fc2b6ff528e1c647e117
andrei-pvl <147505074+andrei-pvl@users.noreply.github.com>
2023-11-09 02:00:56 +0200

    Add files via upload

---
+++ Launcher.cs
@@ -0,0 +1,23 @@
+﻿
+namespace comp
+{
+    class Launcher
+    {
+        public string LaunchOS()
+        {
+            return "Операцiйна система запущена";
+        }
+        public string LaunchProgram()
+        {
+            return "Обробка файлiв/програм запущена";
+        }
+        public string OffProgram()
+        {
+            return "Обробка файлiв/програм вимкнена";
+        }
+        public string OffOS()
+        {
+            return "Операцiйна система вимкнена";
+        }
+    }
+}
---
+++ Switcher.cs
@@ -0,0 +1,18 @@
+﻿
+namespace comp
+{
+    class Switcher
+    {
+        private bool isOn = false;
+        public string On()
+        {
+            isOn = true;
+            return "Комп'ютер ввiмкнуто";
+        }
+        public string Off()
+        {
+            isOn = false;
+            return "Комп'ютер вимкнуто";
+        }
+    }
+}
