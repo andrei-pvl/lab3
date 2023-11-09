87cd4d82f934dcd46f1d91c8f5533f96415ead9d
andrei-pvl <147505074+andrei-pvl@users.noreply.github.com>
2023-11-09 02:02:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using comp;
+
+namespace lb3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IComp computer = SingelComputer.Initialize();
+            Console.WriteLine(computer.ComputerName());
+            Console.WriteLine();
+
+            FacadeComp facadecomp = new FacadeComp();
+            Console.WriteLine(facadecomp.ComputerName());
+            facadecomp.TurnOn();
+            facadecomp.ProgramWork();
+            facadecomp.TurnOff();
+
+            Console.ReadLine();
+        }
+    }
+}
